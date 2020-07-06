be2dc149ac31dad02f1a33c2161831f6996e207c
MitjaHaimila <mitja.haimila@gmail.com>
2020-07-06 14:41:26 +0300

    lisätty introkoodi

--- aws-quiz.py
+++ aws-quiz.py
@@ -1,3 +1,35 @@
+def intro():
+    global pelitila
+    while True:
 
-def funktio():
-    pass
+        try:
+
+            print("Tervetuloa AWS-triviaan!")
+            pelataanko = input("Haluatko aloittaa uuden pelin K/E?")
+
+        except:
+
+            print("Paina joko K aloittaaksesi pelin tai E sulkeaksi ohjelman!")
+            continue
+
+        else:
+            if pelataanko[0].lower() == "k":
+                break
+
+            elif pelataanko[0].lower() == "e":
+                pelitila = False
+                break
+
+
+pelitila = True
+
+while pelitila:
+    pisteet = 0
+
+    intro()
+    
+
+    continue
+
+
+
