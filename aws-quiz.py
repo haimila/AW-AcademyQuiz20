8c27a47c9d80f064bb0c32f37d058360b7da7772
MitjaHaimila <mitja.haimila@gmail.com>
2020-07-06 15:24:39 +0300

    lisätty kaksi kysymystä

--- aws-quiz.py
+++ aws-quiz.py
@@ -4,22 +4,57 @@ def intro():
 
         try:
 
-            print("Tervetuloa AWS-triviaan!")
-            pelataanko = input("Haluatko aloittaa uuden pelin K/E?")
+            print("Welcome to AWS trivia!")
+            pelataanko = input("Do you want to start the game Y/N?")
 
         except:
 
-            print("Paina joko K aloittaaksesi pelin tai E sulkeaksi ohjelman!")
+            print("Input either Y to start or N to quit!")
             continue
 
         else:
-            if pelataanko[0].lower() == "k":
+            if pelataanko[0].lower() == "y":
                 break
 
-            elif pelataanko[0].lower() == "e":
+            elif pelataanko[0].lower() == "n":
                 pelitila = False
                 break
 
+def kysymys1():
+    global pisteet
+    while True:
+        print("Which of these services is global? Choose A, B, or C.")
+        print("A) EC2")
+        print("B) IAM")
+        print("C) VPC")
+        vastaus = input("Your answer: ")
+
+        if vastaus[0].lower() == "b":
+            print("Correct!")
+            pisteet += 1
+            break
+
+        else:
+            print("Wrong answer!")
+            break
+
+def kysymys1():
+    global pisteet
+    while True:
+        print("Which of these is EC2 part of?")
+        print("A) IaaS")
+        print("B) PaaS")
+        print("C) SaaS")
+        vastaus = input("Your answer: ")
+
+        if vastaus[0].lower() == "a":
+            print("Correct!")
+            pisteet += 1
+            break
+
+        else:
+            print("Wrong answer!")
+            break
 
 pelitila = True
 
@@ -27,9 +62,10 @@ while pelitila:
     pisteet = 0
 
     intro()
-    
+    kysymys1()
+    kysymys2()
 
-    continue
+    break
 
 
 
